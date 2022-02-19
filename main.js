54ddf7cc62eaee4f4a03b7a13867f7b65e71e42f
Tsukina-7mochi <tsukina7mochi@gmail.com>
2022-02-19 16:53:31 +0900

    ランダムアクセスの実験を追加

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const variableNum = 1000_0000;
 // 実験する回数
 const experimentNum = 10;
 
+console.log('== AND operation ==');
+
+
 // オペランドの配列2つと結果を格納する配列
 const variableArr1 = new Array(variableNum).fill(0).map(_ => Math.random() < 0.5);
 const variableArr2 = new Array(variableNum).fill(0).map(_ => Math.random() < 0.5);
@@ -18,7 +21,7 @@ for(let i = 0; i < experimentNum; i++) {
 
   const timeEnd = Date.now();
 
-  console.log(`arr#${i}ms: ${timeEnd - timeStart}`);
+  console.log(`arr#${i}: ${timeEnd - timeStart}ms`);
   timeSumArr += timeEnd - timeStart;
 }
 
@@ -33,6 +36,44 @@ for(let i = 0; i < experimentNum; i++) {
 
   for(let i = 0; i < variableNum; i++) {
     resultBit[i] = variableBit1[i] & variableBit2[i];
+  }
+
+  const timeEnd = Date.now();
+
+  console.log(`bit#${i}: ${timeEnd - timeStart}ms`);
+  timeSumBit += timeEnd - timeStart;
+}
+
+
+console.log(`Average(arr): ${timeSumArr / experimentNum}ms`);
+console.log(`Average(bit): ${timeSumBit / experimentNum}ms`);
+
+
+console.log('== Random Access ==');
+
+
+timeSumArr = 0;
+
+for(let i = 0; i < experimentNum; i++) {
+  const timeStart = Date.now();
+
+  for(let i = 0; i < variableNum; i++) {
+    resultArr[i];
+  }
+
+  const timeEnd = Date.now();
+
+  console.log(`arr#${i}: ${timeEnd - timeStart}ms`);
+  timeSumArr += timeEnd - timeStart;
+}
+
+timeSumBit = 0;
+
+for(let i = 0; i < experimentNum; i++) {
+  const timeStart = Date.now();
+
+  for(let i = 0; i < variableNum; i++) {
+    (resultBit[Math.floor(i / 32)] >> (31 - (i % 32))) & 1;
   }
 
   const timeEnd = Date.now();
