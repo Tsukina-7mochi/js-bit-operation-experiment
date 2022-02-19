0323444b75581eed141f60d2016e8dd2cec1cd5f
Tsukina-7mochi <tsukina7mochi@gmail.com>
2022-02-19 18:04:18 +0900

    Typed Arrayでない数値配列についての実験を追加

--- main.js
+++ main.js
@@ -1,87 +1,130 @@
-// 変数の数
-const variableNum = 1000_0000;
+// 値の数
+const valNum = 1000_0000;
 // 実験する回数
 const experimentNum = 10;
 
 console.log('== AND operation ==');
 
 
-// オペランドの配列2つと結果を格納する配列
-const variableArr1 = new Array(variableNum).fill(0).map(_ => Math.random() < 0.5);
-const variableArr2 = new Array(variableNum).fill(0).map(_ => Math.random() < 0.5);
-const resultArr = new Array(variableNum);
-let timeSumArr = 0;
-
+let timeSumBoolArr = 0;
 for(let i = 0; i < experimentNum; i++) {
+  const valBoolArr1 = new Array(valNum).fill(0).map(_ => Math.random() < 0.5);
+  const valBoolArr2 = new Array(valNum).fill(0).map(_ => Math.random() < 0.5);
+  const resultBoolArr = new Array(valNum);
+
   const timeStart = Date.now();
 
-  for(let i = 0; i < variableNum; i++) {
-    resultArr[i] = variableArr1[i] && variableArr2[i];
+  for(let i = 0; i < valBoolArr1.length; i++) {
+    resultBoolArr[i] = valBoolArr1[i] && valBoolArr2[i];
   }
 
   const timeEnd = Date.now();
 
-  console.log(`arr#${i}: ${timeEnd - timeStart}ms`);
-  timeSumArr += timeEnd - timeStart;
+  console.log(`boolean[]#${i}: ${timeEnd - timeStart}ms`);
+  timeSumBoolArr += timeEnd - timeStart;
 }
 
 
-const variableBit1 = new Uint32Array(variableNum).fill(0).map(_ => Math.floor(Math.random() * (2 ** 32)));
-const variableBit2 = new Uint32Array(variableNum).fill(0).map(_ => Math.floor(Math.random() * (2 ** 32)));
-const resultBit = new Uint32Array(variableNum);
-let timeSumBit = 0;
-
+let timeSumBitTyped = 0;
 for(let i = 0; i < experimentNum; i++) {
+  const valBitTyped1 = new Uint32Array(Math.ceil(valNum / 32)).fill(0).map(_ => Math.floor(Math.random() * (2 ** 32)));
+  const valBitTyped2 = new Uint32Array(Math.ceil(valNum / 32)).fill(0).map(_ => Math.floor(Math.random() * (2 ** 32)));
+  const resultBitTyped = new Uint32Array(Math.ceil(valNum / 32));
+
   const timeStart = Date.now();
 
-  for(let i = 0; i < variableNum; i++) {
-    resultBit[i] = variableBit1[i] & variableBit2[i];
+  for(let i = 0; i < valBitTyped1.length; i++) {
+    resultBitTyped[i] = valBitTyped1[i] & valBitTyped2[i];
   }
 
   const timeEnd = Date.now();
 
-  console.log(`bit#${i}: ${timeEnd - timeStart}ms`);
-  timeSumBit += timeEnd - timeStart;
+  console.log(`Uint32Array#${i}: ${timeEnd - timeStart}ms`);
+  timeSumBitTyped += timeEnd - timeStart;
+}
+
+let timeSumNumArr = 0;
+for(let i = 0; i < experimentNum; i++) {
+  const valNumArr1 = new Array(Math.ceil(valNum / 32)).fill(0).map(_ => Math.floor(Math.random() * (2 ** 32)));
+  const valNumArr2 = new Array(Math.ceil(valNum / 32)).fill(0).map(_ => Math.floor(Math.random() * (2 ** 32)));
+  const resultNumArr = new Array(Math.ceil(valNum / 32));
+
+  const timeStart = Date.now();
+
+  for(let i = 0; i < valNumArr1.length; i++) {
+    resultNumArr[i] = valNumArr1[i] & valNumArr2[i];
+  }
+
+  const timeEnd = Date.now();
+
+  console.log(`number[]#${i}: ${timeEnd - timeStart}ms`);
+  timeSumNumArr += timeEnd - timeStart;
 }
 
 
-console.log(`Average(arr): ${timeSumArr / experimentNum}ms`);
-console.log(`Average(bit): ${timeSumBit / experimentNum}ms`);
+console.log(`Average(boolean[]): ${timeSumBoolArr / experimentNum}ms`);
+console.log(`Average(Uint32Array): ${timeSumBitTyped / experimentNum}ms`);
+console.log(`Average(number[]): ${timeSumNumArr / experimentNum}ms`);
 
 
 console.log('== Random Access ==');
 
 
-timeSumArr = 0;
+timeSumBoolArr = 0;
 
 for(let i = 0; i < experimentNum; i++) {
+  const valBoolArr1 = new Array(valNum).fill(0).map(_ => Math.random() < 0.5);
+  let boolVal = false;
+
   const timeStart = Date.now();
 
-  for(let i = 0; i < variableNum; i++) {
-    resultArr[i];
+  for(let i = 0; i < valBoolArr1.length; i++) {
+    boolVal = valBoolArr1[i];
   }
 
   const timeEnd = Date.now();
 
-  console.log(`arr#${i}: ${timeEnd - timeStart}ms`);
-  timeSumArr += timeEnd - timeStart;
+  console.log(`boolean[]#${i}: ${timeEnd - timeStart}ms`);
+  timeSumBoolArr += timeEnd - timeStart;
 }
 
-timeSumBit = 0;
+timeSumBitTyped = 0;
 
 for(let i = 0; i < experimentNum; i++) {
+  const valBitTyped1 = new Uint32Array(Math.ceil(valNum / 32)).fill(0).map(_ => Math.floor(Math.random() * (2 ** 32)));
+  let boolVal = false;
+
   const timeStart = Date.now();
 
-  for(let i = 0; i < variableNum; i++) {
-    (resultBit[Math.floor(i / 32)] >> (31 - (i % 32))) & 1;
+  for(let i = 0; i < valBitTyped1.length; i++) {
+    boolVal = (valBitTyped1[Math.floor(i / 32)] >> (31 - (i % 32))) & 1;
   }
 
   const timeEnd = Date.now();
 
-  console.log(`bit#${i}: ${timeEnd - timeStart}ms`);
-  timeSumBit += timeEnd - timeStart;
+  console.log(`Uint32Array#${i}: ${timeEnd - timeStart}ms`);
+  timeSumBitTyped += timeEnd - timeStart;
+}
+
+timeSumNumArr = 0;
+
+for(let i = 0; i < experimentNum; i++) {
+  const valNumArr1 = new Array(Math.ceil(valNum / 32)).fill(0).map(_ => Math.floor(Math.random() * (2 ** 32)));
+  let boolVal = false;
+
+  const timeStart = Date.now();
+
+  for(let i = 0; i < valNumArr1.length; i++) {
+    boolVal = (valNumArr1[Math.floor(i / 32)] >> (31 - (i % 32))) & 1;
+  }
+
+  const timeEnd = Date.now();
+
+  console.log(`number[]#${i}: ${timeEnd - timeStart}ms`);
+  timeSumNumArr += timeEnd - timeStart;
 }
 
 
-console.log(`Average(arr): ${timeSumArr / experimentNum}ms`);
-console.log(`Average(bit): ${timeSumBit / experimentNum}ms`);
+console.log(`Average(boolean[]): ${timeSumBoolArr / experimentNum}ms`);
+console.log(`Average(Uint32Array): ${timeSumBitTyped / experimentNum}ms`);
+console.log(`Average(number[]): ${timeSumNumArr / experimentNum}ms`);
